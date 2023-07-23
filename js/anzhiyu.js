5a16ceeaee8b5cd4f04070184fe00f98f1109056
zhangjiawei <zhangjw_wb@ehualu.com>
2023-07-23 11:57:06 +0800

    Site updated: 2023-07-23 11:57:05

--- js/anzhiyu.js
+++ js/anzhiyu.js
@@ -181,3 +181,30 @@ var icatessay = {
 icatessay.changeTimeInEssay();
 icatessay.reflashEssayWaterFall();
 // 逻辑
+
+//影院
+function selectVideo(id){
+  var src=$("#video-item-"+id).attr("data-src");
+  $("#video-select").html("<iframe id='video-iframe' src='"+src+"' scrolling='no' border='0' frameborder='no' framespacing='0' allowfullscreen='true'> </iframe>");
+  var iframe = document.getElementById("video-select")
+  if(iframe.attachEvent){
+    iframe.attachEvent("onreadystatechange", function() {
+      if (iframe.readyState === "complete" || iframe.readyState == "loaded") {
+        iframe.detachEvent("onreadystatechange", arguments.callee);
+      if (document.getElementsByClassName('video-mirror').length>0) {
+        console.log("1true")
+        $(".video-mirror").attr("style","transform:scaleX(-1);")
+        }
+      }
+    });
+  }else{
+    iframe.addEventListener("load", function() {
+      this.removeEventListener("load", arguments.call, false);
+    if (document.getElementsByClassName('video-mirror').length>0) {
+      console.log("2true")
+      $(".video-mirror").attr("style","transform:scaleX(-1);")
+    }
+    }, false);
+  }
+}
+selectVideo('0-0')
